aff57bd328db13c1fcc10a891263bf2d5d351aa2
Liu Rui <lovearuis@gmail.com>
2017-03-18 17:21:54 +0800

    Update AruisRouter.java

--- io/vertx/ext/web/AruisRouter.java
+++ io/vertx/ext/web/AruisRouter.java
@@ -1,28 +1,45 @@
-package io.vertx.ext.web;
+package io.vertx.ext.web.impl;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.ext.web.impl.AruisRouterImpl;
+import io.vertx.ext.web.Route;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by liurui on 2017/3/16.
  */
-public interface AruisRouter extends Router {
+public class AruisRouterImpl extends RouterImpl implements io.vertx.ext.web.AruisRouter {
 
-    static AruisRouter router(Vertx vertx) {
-        return new AruisRouterImpl(vertx);
+
+    private final AtomicInteger orderSequence = new AtomicInteger();
+
+    public AruisRouterImpl(Vertx vertx) {
+        super(vertx);
+    }
+
+
+    @Override
+    public Route route() {
+        return new RouteImpl(this, orderSequence.getAndIncrement());
     }
 
     @Override
-    Route route(HttpMethod method, String path);
+    public AruisRouteImpl route(HttpMethod method, String path) {
+        return new AruisRouteImpl(this, orderSequence.getAndIncrement(), method, path);
+    }
 
     /*
     * 此处只Override了 get、post两个方法，如果需要更多的方法支持指定AruisRouteImpl，可以继续Override其他方法
     * */
 
     @Override
-    AruisRoute get(String path);
+    public AruisRouteImpl get(String path) {
+        return route(HttpMethod.GET, path);
+    }
 
     @Override
-    AruisRoute post(String path);
+    public AruisRouteImpl post(String path) {
+        return route(HttpMethod.POST, path);
+    }
 }
